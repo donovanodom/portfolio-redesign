e94417207bfe13c3cac84acef3d19f3320898dbb
donovanodom <divdonis@gmail.com>
2022-03-01 15:18:41 -0800

    blog styling horizontal cards

--- src/Home/BlogCard.js
+++ src/Home/BlogCard.js
@@ -4,31 +4,29 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 
 const BlogCard = ({ image, link, title, description }) => {
-  const cardStyle = {
-    width: "16rem",
-    height: "480px",
-    margin: "10px auto"
-  };
   const bodyStyle = {
     display: "-webkit-box",
-    WebkitLineClamp: "6",
+    WebkitLineClamp: "3",
     WebkitBoxOrient: "vertical",
     overflow: "hidden"
   };
   const titleStyle = {
     display: "-webkit-box",
-    WebkitLineClamp: "2",
+    WebkitLineClamp: "1",
     WebkitBoxOrient: "vertical",
     overflow: "hidden"
   };
   const imageStyle = {
-    height: "180px"
+    position: "absolute",
+    height: "200px",
+    width: "200px",
+    objectFit: "cover"
   };
   return (
     <Col>
-      <Card style={cardStyle}>
+      <Card className="blog-card">
         <Card.Img variant="top" src={image} style={imageStyle} />
-        <Card.Body>
+        <Card.Body style={{ marginLeft: "200px", textAlign: "left" }}>
           <Card.Title style={titleStyle}>{title}</Card.Title>
           <Card.Text style={bodyStyle}>{description}</Card.Text>
           <Button href={link} variant="primary">
